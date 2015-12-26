74a71e1c00d46cd99b8dc62afdc29150967118f7
劉志翔 <seanliu@liuzhixiangdeMacBook-Pro.local>
2015-12-26 22:24:30 +0800

    adaboost

---
+++ adaboost_track2.py
@@ -0,0 +1,32 @@
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+
+data=np.genfromtxt('sample_train_x.csv',dtype='int',delimiter=',')
+train=data[1:,:]#the first row is string
+y=np.genfromtxt('truth_train.csv',dtype='int',delimiter=',')[:,1]
+
+bdt = AdaBoostClassifier(DecisionTreeClassifier(max_depth=10),
+                         algorithm="SAMME",
+                         n_estimators=200)
+bdt.fit(train,y)
+
+#output the evaluattion of testing
+data=np.genfromtxt('sample_test_x.csv',dtype='int',delimiter=',')
+test=data[1:,:]#the first row is string
+y_eval=bdt.predict(test)
+
+INPUT_FILE 	= "ML_final_project/enrollment_test.csv"
+OUTPUT_FILE = "Output/adaboost_tree_track2.csv"
+
+outFile = open(OUTPUT_FILE, 'w')
+
+with open(INPUT_FILE) as inFile:
+    next(inFile)
+    for num, inData in enumerate(inFile, 0):
+        inData  = inData.rstrip()
+        inData  = inData.split(',')
+        outFile.write(inData[0] + ',' + str(y_eval[num]) + '\n')
+
+inFile.close()
+outFile.close()
