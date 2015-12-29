21777b8e6ca1864841b97808e8c35a6399d47cd0
劉志翔 <seanliu@118-161-129-24.dynamic.hinet.net>
2015-12-29 22:04:11 +0800

    random forest

---
+++ randomforest_track2.py
@@ -0,0 +1,54 @@
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import matplotlib.pyplot as plt
+
+data=np.genfromtxt('ML_final_project/sample_train_x.csv',dtype='int',delimiter=',')
+train=data[1:,1:]#the first row is string,the first column is ID
+y=np.genfromtxt('ML_final_project/truth_train.csv',dtype='int',delimiter=',')[:,1]
+
+#the parameters needing to be determined
+max_depth=[10,20,30,40,None]
+ftr_importance=np.array([6,4,0,15,5,10,14,9,11,3,1,16,2,12,8,7,13])#importance of features
+n_feature=range(10,17,1)#number of feature used
+E_oob=[]
+param=[]
+#determine the parameters using the oob
+for d in max_depth:
+	for f in n_feature:
+		temp_train=train[:,ftr_importance[:f]]
+		bdt = RandomForestClassifier(n_estimators=300,max_depth=d,oob_score=True,n_jobs=-1)
+		bdt.fit(temp_train,y)
+		E_oob.append(1-bdt.oob_score_)
+		param.append([d,f])
+
+print('minimum oob is:',min(E_oob))
+param_choose=param[E_oob.index(min(E_oob))]
+print('the param is: ',param_choose)
+
+
+
+
+
+
+
+
+
+#output the evaluattion of testing
+#data=np.genfromtxt('sample_test_x.csv',dtype='int',delimiter=',')
+#test=data[1:,:]#the first row is string
+#y_eval=bdt.predict(test)
+
+#INPUT_FILE 	= "ML_final_project/enrollment_test.csv"
+#OUTPUT_FILE = "Output/adaboost_tree_track2.csv"
+
+#outFile = open(OUTPUT_FILE, 'w')
+
+#with open(INPUT_FILE) as inFile:
+#    next(inFile)
+#    for num, inData in enumerate(inFile, 0):
+#        inData  = inData.rstrip()
+#        inData  = inData.split(',')
+#        outFile.write(inData[0] + ',' + str(y_eval[num]) + '\n')
+
+#inFile.close()
+#outFile.close()
--- rf_best_param_track1.py
+++ rf_best_param_track1.py
@@ -1,23 +1,24 @@
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+import matplotlib.pyplot as plt
 
 data=np.genfromtxt('ML_final_project/sample_train_x.csv',dtype='int',delimiter=',')
 train=data[1:,1:]#the first row is string,the first column is ID
 y=np.genfromtxt('ML_final_project/truth_train.csv',dtype='int',delimiter=',')[:,1]
+ftr_importance=np.array([6,4,0,15,5,10,14,9,11,3,1,16,2,12,8,7,13])#importance of features
 
-bdt = AdaBoostClassifier(DecisionTreeClassifier(max_depth=10),
-                         algorithm="SAMME",
-                         n_estimators=200)
-bdt.fit(train,y)
-print('score is: ',bdt.score(train,y))#score=accuracy,not error rate
-output the evaluattion of testing
-data=np.genfromtxt('sample_test_x.csv',dtype='int',delimiter=',')
-test=data[1:,1:]#the first row is string,the first column is ID
-y_eval=bdt.predict_proba(test)
+temp_train=train[:,ftr_importance[:13]]
+bdt = RandomForestClassifier(n_estimators=300,max_depth=10,oob_score=True,n_jobs=-1)
+bdt.fit(temp_train,y)
+
+#test
+data=np.genfromtxt('ML_final_project/sample_test_x.csv',dtype='int',delimiter=',')
+test=data[1:,1:]
+temp_test=test[:,ftr_importance[:13]]
+y_eval=bdt.predict_proba(temp_test)
 
 INPUT_FILE 	= "ML_final_project/enrollment_test.csv"
-OUTPUT_FILE = "Output/adaboost_tree_track1.csv"
+OUTPUT_FILE = "Output/rf_best_param_track1.csv"
 
 outFile = open(OUTPUT_FILE, 'w')
 
