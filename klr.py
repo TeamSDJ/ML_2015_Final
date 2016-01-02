8f5786530b351a5b4f882cad95c337a72978e7b6
jeffrey82221 <jeffrey82221@gmail.com>
2016-01-02 16:42:26 +0800

    upload for changing

--- klr.py
+++ klr.py
@@ -16,36 +16,32 @@ import matplotlib.pyplot as plt
 #train_data = [[float(element) for element in line.split()] for line in trainfile]
 #test_data = [[float(element) for element in line.split()] for line in testfile]
 
-data = np.genfromtxt('sample_train_x.txt', delimiter=',')
-truth = np.genfromtxt('truth_train.txt', delimiter=',')
+train_size = 10000
+val_size = 1000
+data = np.matrix(np.genfromtxt('sample_train_x.txt', delimiter=',')[1:,1:])
+truth = np.matrix(np.genfromtxt('truth_train.txt', delimiter=',')[:,1:])
 
-
-
-train_x = np.matrix(data[1:10001,1:])
-train_y = np.matrix(truth[:10000,1:])*2-1
-
-train_x = train_x/train_x.sum(axis = 0)
-
+data = data/data.sum(axis=0)
+truth = truth*2-1
+train_x = data[:train_size,:]
+train_y = truth[:train_size,:]
 
 np.shape(train_x)
 np.shape(train_y)
-#train_x = np.matrix(train_data)[:,:2]
-#train_y = np.matrix(train_data)[:,2:]
 
-#test_x = np.matrix(test_data)[:,:2]
-#test_y = np.matrix(test_data)[:,2:]
-
+val_x = data[train_size:train_size+val_size,:]
+val_y = data[train_size:train_size+val_size,:]
 
 dim = train_x.shape[1]
 N = train_x.shape[0]
 
 #dim = test_x.shape[1]
-#M = test_x.shape[0]
+M = val_x.shape[0]
 
-lemda = 10000
-gama = 10000000000
+print "train_size = ",N," val size = ",M 
 
-
+lemda = 1000
+gama = 1000000000
 
 #target = 1+2*np.random.randint(-1,high=1,size = (N,1))
 
@@ -55,8 +51,8 @@ with tf.device('/gpu:0'):
     y = tf.constant(train_y,dtype=tf.float32) #(dim,1)
     x = tf.constant(train_x,dtype=tf.float32) #(N,dim)
 
-    #y_t = tf.constant(test_y,dtype=tf.float32)
-    #x_t = tf.constant(test_x,dtype=tf.float32)
+    y_v = tf.constant(val_y,dtype=tf.float32)
+    x_v = tf.constant(val_x,dtype=tf.float32)
     #XXX formatting the kernel of training data:
     x_2 = tf.mul(x,x) #(N,d)
     xxT = tf.matmul(x,tf.transpose(x,perm=(1,0))) #(N,N)
@@ -67,30 +63,29 @@ with tf.device('/gpu:0'):
     kernel = tf.exp(tf.mul(tf.constant([-gama],dtype=tf.float32),tf.add(tmp1,tmp2)))
 
 
-#XXX formating the kernel of testing data: (M,N)
-#xm_2 = tf.mul(x_t,x_t) #(M,d)
-#xn_2 = tf.mul(x,x) #(N,d)
+    #XXX formating the kernel of validation data: (M,N)
+    xm_2 = tf.mul(x_v,x_v) #(M,d)
+    xn_2 = tf.mul(x,x) #(N,d)
 
-#xxTmn = tf.matmul(x_t,tf.transpose(x)) #(M,N)
+    xxTmn = tf.matmul(x_v,tf.transpose(x,perm=(1,0))) #(M,N)
 
-#onesMd = tf.constant(np.ones((M,dim)),dtype=tf.float32) #(M,d)
-#onesNd = tf.constant(np.ones((N,dim)),dtype=tf.float32) #(M,d)
-#xxs_xm = tf.matmul(onesNd,tf.transpose(xm_2))
-#xxs_xn = tf.matmul(onesMd,tf.transpose(xn_2))
+    onesMd = tf.constant(np.ones((M,dim)),dtype=tf.float32) #(M,d)
+    onesNd = tf.constant(np.ones((N,dim)),dtype=tf.float32) #(M,d)
+    xxs_xm = tf.matmul(onesNd,tf.transpose(xm_2,perm=(1,0)))
+    xxs_xn = tf.matmul(onesMd,tf.transpose(xn_2,perm=(1,0)))
 
-#test_tmp1 = tf.add(xxs_xn,tf.transpose(xxs_xm))
-#test_tmp2 = tf.mul(tf.constant([-2.],dtype=tf.float32),xxTmn)
+    val_tmp1 = tf.add(xxs_xn,tf.transpose(xxs_xm,perm=(1,0)))
+    val_tmp2 = tf.mul(tf.constant([-2.],dtype=tf.float32),xxTmn)
 
-#test_kernel = tf.exp(tf.mul(tf.constant([-gama],dtype=tf.float32),tf.add(test_tmp1,test_tmp2)))
+    val_kernel = tf.exp(tf.mul(tf.constant([-gama],dtype=tf.float32),tf.add(val_tmp1,val_tmp2)))
 
 
 with tf.device('/cpu:0'):
     #XXX setup the variables
     betas = tf.Variable(tf.zeros([N, 1],dtype=tf.float32))
-    #betasT = tf.transpose(betas,perm=(1,0))
     #XXX kernel placeholder
     kernel_holder = tf.placeholder(tf.float32,shape=(N,N))
-    #test_kernel_holder = tf.placeholder(tf.float32,shape=(M,N))
+    val_kernel_holder = tf.placeholder(tf.float32,shape=(M,N))
 
 with tf.device('/gpu:0'):
     #the constants of equation
@@ -103,29 +98,31 @@ with tf.device('/gpu:0'):
     #second_term = tf.reduce_sum(tf.square(tf.sub(y,second_term_tmp)),0)
     loss = tf.add(first_term,tf.reduce_sum(second_term,0))
     #loss = tf.add(first_term,second_term)
-    optimizer = tf.train.AdamOptimizer(0.01).minimize(loss)
+    optimizer = tf.train.AdamOptimizer(0.001).minimize(loss)
     prediction = tf.sign(tf.matmul(kernel_holder,betas))
-#test_prediction = tf.sign(tf.matmul(test_kernel_holder,betas))
+    val_prediction = tf.sign(tf.matmul(val_kernel_holder,betas))
 
 with tf.Session() as session:
     kernel_variable = kernel.eval()
-
+    #val_kernel_variable = val_kernel.eval()
 
 print "kernel complete!"
-#with tf.Session() as session:
-#    test_kernel_variable = test_kernel.eval()
-
+with tf.Session() as session:
+    val_kernel_variable = val_kernel.eval()
+print "validation kernel complete!"
 #np.shape(test_kernel_variable)
-num_steps = 10000
+num_steps = 10
 with tf.Session() as session:
     tf.initialize_all_variables().run()
     for step in range(num_steps):
-        _= session.run([optimizer], feed_dict={kernel_holder:kernel_variable})
+        _= session.run([optimizer], feed_dict={kernel_holder:kernel_variable,val_kernel_holder:val_kernel_variable})
         if step%10==0:
 	    #p=prediction.eval(feed_dict={kernel_holder:kernel_variable})
             #print type(p)
-	    _,p= session.run([optimizer,prediction], feed_dict={kernel_holder:kernel_variable})
-	    txt = " Ein = "+str(100*np.sum(p!=train_y)/float(p.shape[0]))#+" Eout = "+str(100*np.sum(tp!=test_y)/float(tp.shape[0]))
+	    _,p,vp= session.run([optimizer,prediction,val_prediction], feed_dict={kernel_holder:kernel_variable,val_kernel_holder:val_kernel_variable})
+	    print vp
+	    print val_y
+	    txt = "Ein = "+str(100*np.sum(p!=train_y)/train_size) + " Eout = "+str(100*np.sum(vp!=val_y)/val_size)
             print txt
             #time.sleep(0.5)
             #print '\r','loss = ',l,'accuracy = ',100*np.sum(p==target)/float(p.shape[0])
