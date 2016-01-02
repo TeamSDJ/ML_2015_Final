c8bba2c79b948c153edb0be27e1b8208d3323167
Jeffrey Lin <jeffrey82221@gmail.com>
2016-01-02 21:37:12 +0800

    split kernels

--- klr_test.py
+++ klr_test.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 lemda = 0.5
 gama = 1000000000
 train_size = 15000
-val_size = 15000
+
 data = np.matrix(np.genfromtxt('sample_train_x.txt', delimiter=',')[1:,1:])
 truth = np.matrix(np.genfromtxt('truth_train.txt', delimiter=',')[:,1:])
 test_data = np.matrix(np.genfromtxt('sample_test_x.txt', delimiter=',')[1:,1:])
@@ -25,66 +25,99 @@ train_y = truth[:train_size,:]
 np.shape(train_x)
 np.shape(train_y)
 
-test_x = test_data
-
+test_x1 = test_data[:15000,:]
+test_x2 = test_data[15000:,:]
 dim = train_x.shape[1]
 N = train_x.shape[0]
 
 
-M = test_x.shape[0]
+M1 = test_x1.shape[0]
+M2 = test_x2.shape[0]
+print "train_size = ",N," test size = ",M1+M2
 
-print "train_size = ",N," val size = ",M
-#target = 1+2*np.random.randint(-1,high=1,size = (N,1))
-
-
+# the kernel for the 1st part test data
 with tf.device('/cpu:0'):
     #XXX setup the tf const for the training data
 
     x = tf.constant(train_x,dtype=tf.float32) #(N,dim)
-    x_t = tf.constant(test_x,dtype=tf.float32)
+    x_t1 = tf.constant(test_x1,dtype=tf.float32)
 
     #XXX formating the kernel of validation data: (M,N)
-    xm_2 = tf.mul(x_t,x_t) #(M,d)
+    xm1_2 = tf.mul(x_t1,x_t1) #(M,d)
     xn_2 = tf.mul(x,x) #(N,d)
 
-    xxTmn = tf.matmul(x_t,tf.transpose(x,perm=(1,0))) #(M,N)
+    xxTm1n = tf.matmul(x_t1,tf.transpose(x,perm=(1,0))) #(M,N)
 
-    onesMd = tf.constant(np.ones((M,dim)),dtype=tf.float32) #(M,d)
+    onesM1d = tf.constant(np.ones((M1,dim)),dtype=tf.float32) #(M,d)
     onesNd = tf.constant(np.ones((N,dim)),dtype=tf.float32) #(M,d)
-    xxs_xm = tf.matmul(onesNd,tf.transpose(xm_2,perm=(1,0)))
-    xxs_xn = tf.matmul(onesMd,tf.transpose(xn_2,perm=(1,0)))
+    xxs_xm1 = tf.matmul(onesNd,tf.transpose(xm1_2,perm=(1,0)))
+    xxs_x1n = tf.matmul(onesM1d,tf.transpose(xn_2,perm=(1,0)))
 
-    test_tmp1 = tf.add(xxs_xn,tf.transpose(xxs_xm,perm=(1,0)))
-    test_tmp2 = tf.mul(tf.constant([-2.],dtype=tf.float32),xxTmn)
+    test1_tmp1 = tf.add(xxs_x1n,tf.transpose(xxs_xm1,perm=(1,0)))
+    test1_tmp2 = tf.mul(tf.constant([-2.],dtype=tf.float32),xxTm1n)
 
-    test_kernel = tf.exp(tf.mul(tf.constant([-gama],dtype=tf.float32),tf.add(test_tmp1,test_tmp2)))
+    test1_kernel = tf.exp(tf.mul(tf.constant([-gama],dtype=tf.float32),tf.add(test1_tmp1,test1_tmp2)))
+
+# the kernel for the 2nd part test data
+with tf.device('/cpu:0'):
+    #XXX setup the tf const for the training data
+
+    x = tf.constant(train_x,dtype=tf.float32) #(N,dim)
+    x_t2 = tf.constant(test_x2,dtype=tf.float32)
+
+    #XXX formating the kernel of validation data: (M,N)
+    xm2_2 = tf.mul(x_t2,x_t2) #(M,d)
+    xn_2 = tf.mul(x,x) #(N,d)
+
+    xxTm2n = tf.matmul(x_t2,tf.transpose(x,perm=(1,0))) #(M,N)
+
+    onesM2d = tf.constant(np.ones((M2,dim)),dtype=tf.float32) #(M,d)
+    onesNd = tf.constant(np.ones((N,dim)),dtype=tf.float32) #(M,d)
+    xxs_xm2 = tf.matmul(onesNd,tf.transpose(xm2_2,perm=(1,0)))
+    xxs_x2n = tf.matmul(onesM2d,tf.transpose(xn_2,perm=(1,0)))
+
+    test2_tmp1 = tf.add(xxs_x2n,tf.transpose(xxs_xm2,perm=(1,0)))
+    test2_tmp2 = tf.mul(tf.constant([-2.],dtype=tf.float32),xxTm2n)
+
+    test2_kernel = tf.exp(tf.mul(tf.constant([-gama],dtype=tf.float32),tf.add(test2_tmp1,test2_tmp2)))
+
 
 
 with tf.device('/cpu:0'):
     #XXX setup the variables
     betas = tf.Variable(tf.zeros([N, 1],dtype=tf.float32))
     #XXX kernel placeholder
-    test_kernel_holder = tf.placeholder(tf.float32,shape=(M,N))
+    test1_kernel_holder = tf.placeholder(tf.float32,shape=(M1,N))
+    test2_kernel_holder = tf.placeholder(tf.float32,shape=(M2,N))
+
 
 with tf.device('/cpu:0'):
+    test1_prediction = tf.sign(tf.matmul(test1_kernel_holder,betas))
+    test2_prediction = tf.sign(tf.matmul(test2_kernel_holder,betas))
 
-    test_prediction = tf.sign(tf.matmul(test_kernel_holder,betas))
+
 saver = tf.train.Saver()
 
 with tf.Session() as session:
-    test_kernel_variable = test_kernel.eval()
+    test1_kernel_variable = test1_kernel.eval()
+
+with tf.Session() as session:
+    test2_kernel_variable = test2_kernel.eval()
 print "validation kernel complete!"
-#np.shape(test_kernel_variable)
 num_steps = 1
 with tf.Session() as session:
     saver.restore(session,"klr_model.ckpt")
     for step in range(num_steps):
         if step%1==0:
-	    tp= session.run(test_prediction, feed_dict={test_kernel_holder:test_kernel_variable})
-        print tp
+	    tp1,tp2= session.run([test1_prediction,test2_prediction], feed_dict={test1_kernel_holder:test1_kernel_variable,test2_kernel_holder:test2_kernel_variable})
 
-tp =(tp+1)/2
+
+print "result generated"
+tp1 =(tp1+1)/2
+tp2 =(tp2+1)/2
 result_file = open('result.txt','w')
-for i in range(tp.shape[0]):
-    result_file.write(str(int(tp[i,0]))+'\n')
+for i in range(tp1.shape[0]):
+    result_file.write(str(int(tp1[i,0]))+'\n')
+for i in range(tp2.shape[0]):
+    result_file.write(str(int(tp2[i,0]))+'\n')
 result_file.close()
