4e881b425b8c91c06c340a4e83767c66442c86f1
Jeffrey Lin <jeffrey82221@gmail.com>
2016-01-02 21:19:41 +0800

    rename val to test

--- klr_test.py
+++ klr_test.py
@@ -25,13 +25,13 @@ train_y = truth[:train_size,:]
 np.shape(train_x)
 np.shape(train_y)
 
-val_x = test_data
+test_x = test_data
 
 dim = train_x.shape[1]
 N = train_x.shape[0]
 
 
-M = val_x.shape[0]
+M = test_x.shape[0]
 
 print "train_size = ",N," val size = ",M
 #target = 1+2*np.random.randint(-1,high=1,size = (N,1))
@@ -41,49 +41,46 @@ with tf.device('/cpu:0'):
     #XXX setup the tf const for the training data
 
     x = tf.constant(train_x,dtype=tf.float32) #(N,dim)
-
-    #y_v = tf.constant(val_y,dtype=tf.float32)
-    x_v = tf.constant(val_x,dtype=tf.float32)
+    x_t = tf.constant(test_x,dtype=tf.float32)
 
     #XXX formating the kernel of validation data: (M,N)
-    xm_2 = tf.mul(x_v,x_v) #(M,d)
+    xm_2 = tf.mul(x_t,x_t) #(M,d)
     xn_2 = tf.mul(x,x) #(N,d)
 
-    xxTmn = tf.matmul(x_v,tf.transpose(x,perm=(1,0))) #(M,N)
+    xxTmn = tf.matmul(x_t,tf.transpose(x,perm=(1,0))) #(M,N)
 
     onesMd = tf.constant(np.ones((M,dim)),dtype=tf.float32) #(M,d)
     onesNd = tf.constant(np.ones((N,dim)),dtype=tf.float32) #(M,d)
     xxs_xm = tf.matmul(onesNd,tf.transpose(xm_2,perm=(1,0)))
     xxs_xn = tf.matmul(onesMd,tf.transpose(xn_2,perm=(1,0)))
 
-    val_tmp1 = tf.add(xxs_xn,tf.transpose(xxs_xm,perm=(1,0)))
-    val_tmp2 = tf.mul(tf.constant([-2.],dtype=tf.float32),xxTmn)
+    test_tmp1 = tf.add(xxs_xn,tf.transpose(xxs_xm,perm=(1,0)))
+    test_tmp2 = tf.mul(tf.constant([-2.],dtype=tf.float32),xxTmn)
 
-    val_kernel = tf.exp(tf.mul(tf.constant([-gama],dtype=tf.float32),tf.add(val_tmp1,val_tmp2)))
+    test_kernel = tf.exp(tf.mul(tf.constant([-gama],dtype=tf.float32),tf.add(test_tmp1,test_tmp2)))
 
 
 with tf.device('/cpu:0'):
     #XXX setup the variables
     betas = tf.Variable(tf.zeros([N, 1],dtype=tf.float32))
     #XXX kernel placeholder
-    val_kernel_holder = tf.placeholder(tf.float32,shape=(M,N))
+    test_kernel_holder = tf.placeholder(tf.float32,shape=(M,N))
 
 with tf.device('/cpu:0'):
 
-    val_prediction = tf.sign(tf.matmul(val_kernel_holder,betas))
+    test_prediction = tf.sign(tf.matmul(test_kernel_holder,betas))
 saver = tf.train.Saver()
 
 with tf.Session() as session:
-    val_kernel_variable = val_kernel.eval()
+    test_kernel_variable = test_kernel.eval()
 print "validation kernel complete!"
 #np.shape(test_kernel_variable)
 num_steps = 1
 with tf.Session() as session:
-
     saver.restore(session,"klr_model.ckpt")
     for step in range(num_steps):
         if step%1==0:
-	    tp= session.run(val_prediction, feed_dict={val_kernel_holder:val_kernel_variable})
+	    tp= session.run(test_prediction, feed_dict={test_kernel_holder:test_kernel_variable})
         print tp
 
 tp =(tp+1)/2
